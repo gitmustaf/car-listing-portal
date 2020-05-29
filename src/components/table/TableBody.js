7d7d6f4cc950d4c010e332fff2e471544f80404c
soumenpasari <soumenpasari849@gmail.com>
2020-05-30 01:55:01 +0530

    feat:Initialising api call to get data

--- src/components/table/TableBody.js
+++ src/components/table/TableBody.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useEffect,useState } from 'react'
 import TableRows from '../table/TableRows'
+import axios from 'axios'
 
 const TableBody = ({setPopUpActiveClass}) => {
+    const [carData,setCarData] = useState([]);
     const setPopUpClassToSet = (isActive) => {
         setPopUpActiveClass(isActive);
-        console.log(`isactive body ${isActive}`)
     }
+    useEffect(() => {
+        const fetchData = async () => {
+          const result = await axios(
+            'https://krabhp3hz3.execute-api.us-east-1.amazonaws.com/car/info',
+            {
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                  }
+            }
+          )
+     
+        //   setCarData(result.data);
+        };
+     
+        fetchData();
+        console.log(carData);
+      });
     return (
         <tbody>
             <TableRows setPopUpClassToSet={setPopUpClassToSet} />
